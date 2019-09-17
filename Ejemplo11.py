224d9fa0810b0407fc0a0c4253d780ae571f29cd
djob <valadezlender@gmail.com>
2019-09-17 16:35:02 -0500

    tarea ejemplo 10 y 11

---
+++ Ejemplo11.py
@@ -0,0 +1,7 @@
+A = int(input("Dame un numero"))
+B = int(input("Dame un numero"))
+C = int(input("Dame un numero"))
+D = int(input("Dame un numero"))
+
+print ("Los numeros dados son { D } { C } { B } y { A } ")
+
