387d3ef2d927a5970da2da366a8e9cde5265b413
djob <valadezlender@gmail.com>
2019-09-18 23:28:58 -0500

    ejemplo 13 realizado

---
+++ libro/Ejemplo13.py
@@ -0,0 +1,12 @@
+print ("Programa para promedios")
+
+CAL1 = int(input("Dame la primera calificacion "))
+CAL2 = int(input("Dame la segunda calificacion "))
+CAL3 = int(input("Dame la tercera calificacion "))
+CAL4 = int(input("Dame la cuarta calificacion "))
+CAL5 = int(input("Dame la quinta calificacion "))
+
+
+PRO = ( CAL1 + CAL2 + CAL3 + CAL4 + CAL5 )/5
+
+print  ( PRO, )
