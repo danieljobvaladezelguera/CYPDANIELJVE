1c3f17a9fdfdab53d25a0110b63855ccbe7896d1
djob <valadezlender@gmail.com>
2019-09-19 00:26:48 -0500

    Ejemplo 15 realizado

---
+++ libro/Ejemplo15.py
@@ -0,0 +1,10 @@
+print ("Programa para sacar area y perimetro de un rectangulo ")
+
+BASE = int(input("Escribe la base "))
+ALTU = int(input("Escribe la altura "))
+
+AREA = ( BASE * ALTU )
+PERI =  2 * ( BASE + ALTU )
+
+print  ( AREA )
+print  ( PERI )
